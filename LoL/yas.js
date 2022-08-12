53a638d846d4f69fa310f294e1a2910aba02f78f
Quoc Trinh Trinh <89927336+QuocTrinh-coder@users.noreply.github.com>
2022-08-12 09:53:01 +0000

    adding yone and yas

--- LoL/yas.js
+++ LoL/yas.js
@@ -1,6 +1,8 @@
 //Yasuo Game ____________________________________________________________________________________
 //" " = space bar in keypress
 // loading prologue
+let yas = document.querySelector(".yas-pic");
+let yone = document.querySelector(".yone-pic");
 let container = document.querySelector(".context");
 var _CONTENT = [
   "[ Ionia ]",
@@ -8,8 +10,11 @@ var _CONTENT = [
   "[ Surrounded by treacherous seas, Ionia is composed of a number of allied provinces scattered across a massive archipelago, known to many as the 'First Lands'. ]",
   "[ The place where material and spirit realms tends to be more permeable here, especially in the wild forests and mountains. ]",
   " [ The place where it's blessed with rich resources and a longstanding history. ]",
-  "[ And it is also the home of many magical creatures with some of the most powerful races including the mages, demi-human, and the ninjas whose vowed to protect the village. ]"
-];
+  "[ And it is also the home of many magical creatures with some of the most powerful races including the mages, demi-human, and the ninjas whose vowed to protect the village. ]",
+  "[ In this village, there are 2 siblings that villagers gossip about. ]",
+  "[ Yone, the older brother who has everything. Respects, Admires and Intelligences. ]",
+  "[ And Yasuo, the little brother who hated by his village. In best case, his existance is Gods' mistake, and in worst case, he is the mistake that can't be undone. ]",
+  "[ However, Yone and Yasuo also have each other back. And soon, they both attended a famous martial art school in the village. ]",]
 
 // Current sentence being processed
 var _PART = 0;
@@ -29,6 +34,13 @@ function Type() {
   _ELEMENT.innerHTML = text;
   _PART_INDEX++;
 
+  if (_PART == 8) {
+    yas.classList.remove("hidden");
+  }
+  if (_PART == 7) {
+    yone.classList.remove("hidden");
+  }
+  
   // If full sentence has been displayed then start to delete the sentence after some time
   if (text === _CONTENT[_PART]) {
     // Hide the cursor
@@ -41,7 +53,7 @@ function Type() {
 }
 
 // Implements deleting effect
-function Delete() {
+async function Delete() {
   // Get substring with 1 characater deleted
   var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
   _ELEMENT.innerHTML = text;
@@ -52,10 +64,11 @@ function Delete() {
     clearInterval(_INTERVAL_VAL);
 
     // If current sentence was last then stop
+    if (_PART === 5) {
+      await sleep(3.5);// stop at the last sentence without resetting the sentence counting again
+    }
     if (_PART == (_CONTENT.length - 1)) {
-      _PART = _PART; // stop at the last sentence without resetting the sentence counting again
-      container.classList.add("hidden");
-      button.classList.remove("hidden");
+      _PART = _PART;
       return;
     }
     else
@@ -73,3 +86,7 @@ function Delete() {
 
 // Start the typing effect on load
 _INTERVAL_VAL = setInterval(Type, 10);
+
+async function sleep(seconds) {
+  return new Promise((resolve) => setTimeout(resolve, seconds*1000));
+}
